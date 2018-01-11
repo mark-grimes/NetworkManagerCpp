149f1d4fcbb4151af480f045686e204d4f11defc
Mark Grimes <mark.grimes@rymapt.com>
2018-01-11 10:49:25 +0000

    Remove creation of string from NM_MAJOR_VERSION etc

--- tests/src/test_Client.cpp
+++ tests/src/test_Client.cpp
@@ -1,6 +1,5 @@
 #include "libnm/Client.h"
 #include "catch.hpp"
-#include <nm-version.h>
 
 SCENARIO( "Test that the libnm::Client class performs as expected", "[Client]" )
 {
@@ -8,7 +7,6 @@ SCENARIO( "Test that the libnm::Client class performs as expected", "[Client]" )
 	{
 		libnm::Client client;
 		const char* version=nullptr;
-		std::string libnmVersion=std::to_string(NM_MAJOR_VERSION)+"."+std::to_string(NM_MINOR_VERSION)+"."+std::to_string(NM_MICRO_VERSION);
 		CHECK_NOTHROW( version=client.getVersion() );
 		CHECK( version != nullptr );
 	}
