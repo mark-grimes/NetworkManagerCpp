50d044efbee1e1ce5dd9b85bd23f0389dd226639
Mark Grimes <mark.grimes@rymapt.com>
2018-01-11 11:29:21 +0000

    Put constructor in REQUIRE_NOTHROW block

--- tests/src/test_Client.cpp
+++ tests/src/test_Client.cpp
@@ -1,13 +1,18 @@
 #include "libnm/Client.h"
+#include <memory>
 #include "catch.hpp"
 
 SCENARIO( "Test that the libnm::Client class performs as expected", "[Client]" )
 {
 	WHEN( "Test that Client::getVersion() returns something valid" )
 	{
-		libnm::Client client;
+		// Need to use pointer so that I can wrap constructor in CHECK_NOTHROW block
+		std::unique_ptr<libnm::Client> pClient;
 		const char* version=nullptr;
-		CHECK_NOTHROW( version=client.getVersion() );
+		REQUIRE_NOTHROW( pClient.reset(new libnm::Client) );
+		REQUIRE( pClient != nullptr );
+		CHECK_NOTHROW( version=pClient->getVersion() );
 		CHECK( version != nullptr );
+		// TODO check the version string is sensible
 	}
 } // end of 'SCENARIO ... libnm::Client'
